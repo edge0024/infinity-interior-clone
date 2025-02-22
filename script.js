d959752a6b98b3251531945c980ac65ec0be5b4d
edge0024 <apu7rv@gmail.com>
2025-02-22 11:56:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-// Smooth scrolling for anchor links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-        behavior: 'smooth'
-      });
-    });
-  });
+const carousel = document.querySelector(".carousel");
+
+function moveCarousel() {
+    let firstImage = carousel.children[0];
+    carousel.style.transition = "transform 1s linear";
+    carousel.style.transform = "translateX(-20%)";
+
+    setTimeout(() => {
+        carousel.appendChild(firstImage);
+        carousel.style.transition = "none";
+        carousel.style.transform = "translateX(0)";
+    }, 1000);
+}
+
+setInterval(moveCarousel, 2000);
